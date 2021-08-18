9dbae5df14b31deafac3268fe8ec1d3813d9b906
samjbruce <samueljamesbruce@gmail.com>
2021-08-18 10:35:24 -0500

    adds routes and controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,44 @@
 class ProductsController < ApplicationController
+
+  def index
+    products = Product.all
+    render json: products
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url]
+    )
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    if product.save
+      render json: product
+    else
+      render json: { errors: product.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: { message: "Product Deleted" }
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_144550) do
+ActiveRecord::Schema.define(version: 2021_08_18_151923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_144550) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
